01e46752a196c08a03f30c4b3e8429fb5221eca4
Container 9727 cc-9727@jcdn.de <script@jcdn.de>
2022-04-28 12:12:46 +0200

    Fixed some emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF['jar_pugtemplate'] = array(
 	'title' => 'PUG Template',
 	'description' => 'Tools for rendering PUG templates in TYPO3',
 	'category' => 'plugin',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'typo3' => '10.4.1-11.5.99',
 			'php' => '7.4.0-7.4.999',
-			'jar_utilities' => '1.0.0'
+			'jar_utilities' => '1.0.0-1.0.99'
 		),
 		'conflicts' => array(
 		),
